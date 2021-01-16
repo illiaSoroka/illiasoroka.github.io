2fa4b2e70dee70988142ef5fe4a29a6d57c42ed7
Illia Soroka <ilycha92@gmail.com>
2021-01-15 22:25:22 -0600

    add new web site

--- fitlifealex/js/common.js
+++ fitlifealex/js/common.js
@@ -1,7 +1,15 @@
-$(function () {
+const slider = document.querySelector('.slider');
 
-  // Custom JS
-  // alert("hello World!");
+slider.addEventListener('click', (event) => {
+	if (event.target.tagName == 'IMG') {
 
-});
+		if (event.target.className = '.img-width') {
+			let li = event.target.parentNode;
+			let nextLi = li.previousElementSibling;
+			let ul = li.parentNode;
 
+			ul.insertBefore(li, nextLi);
+
+		}
+	}
+})
